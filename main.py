a53842bb54d94166d3718bcea06d368cd0a962ec
NiamhK07 <113034875+Niamh-Keenan@users.noreply.github.com>
2022-10-09 02:23:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+(random.randint(1,6))
+
+a= random.randint (1,6)
+ 
+input("Enter a number from 1 to 6: ")
+
+if input == a:
+
+  print("Correct: %d" %(a))
+  
+else input != a:
+
+  print("Incorrect,the number was:",(a))
+  
